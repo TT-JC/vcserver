1747bf389648a09d37697418d8b51352389e72ed
TT-JC <1016399120@qq.com>
2020-07-30 00:58:54 +0800

    debug

--- app/util/dispatcher.js
+++ app/util/dispatcher.js
@@ -1,6 +1,9 @@
 var crc = require('crc');
 
-module.exports.dispatch = function(uid, connectors) {
-	var index = Math.abs(crc.crc32(uid)) % connectors.length;
-	return connectors[index];
+module.exports.dispatch = function (uid, connectors) {
+    console.info(connectors);
+    var count = connectors.length;
+    console.info("count=", count);
+    var index = Math.abs(crc.crc32(uid)) % connectors.length;
+    return connectors[index];
 };
