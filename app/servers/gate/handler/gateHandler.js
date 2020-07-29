6704ed1fbf5fc72a4b76cd79a93aa210b2d926d1
TT-JC <1016399120@qq.com>
2020-07-29 23:27:24 +0800

    debug

--- app/servers/gate/handler/gateHandler.js
+++ app/servers/gate/handler/gateHandler.js
@@ -2,12 +2,12 @@ var dispatcher = require('../../../util/dispatcher');
 var accountDao = require('../../../dao/accountDao');
 var async = require('async');
 
-module.exports = function(app) {
-	return new Handler(app);
+module.exports = function (app) {
+    return new Handler(app);
 };
 
-var Handler = function(app) {
-	this.app = app;
+var Handler = function (app) {
+    this.app = app;
 };
 
 /**
@@ -18,42 +18,44 @@ var Handler = function(app) {
  * @param {Function} next next stemp callback
  *
  */
-Handler.prototype.queryEntry = function(msg, session, next) {
-	var self = this;
-	var uid, host, port;
-	async.waterfall([
-		function(cb) {
-			accountDao.getAccountInfo(msg.name,function(err, player) {
-				cb(err,player);
-			});
-		},
-		function(player, cb) {
-			if(!player){
-				accountDao.createAccount(msg.name,function(err, player) {
-					cb(err,player.id);
-				});
-			}else cb(err,player.id)
-		},
-		function(id,cb) {
-			uid = id;
-			var connectors = self.app.getServersByType('connector');
-			if(!connectors || connectors.length === 0) {
-				next(null, {
-					code: 500
-				});
-				return;
-			}
-			// select connector
-			var res = dispatcher.dispatch(uid, connectors);
-			host = res.host;
-			port = res.clientPort;
-			cb(err);
-		}], function(err) {
-			if(err) {
-				next(err, {code: 501});
-				return;
-			}
-			next(null, {code: 200, uid: uid, host: host, port: port});
-		}
-	);
+Handler.prototype.queryEntry = function (msg, session, next) {
+    var self = this;
+    var uid, host, port;
+    async.waterfall([
+        function (cb) {
+            accountDao.getAccountInfo(msg.name, function (err, player) {
+                cb(err, player);
+            });
+        },
+        function (player, cb) {
+            if (!player) {
+                accountDao.createAccount(msg.name, function (err, player) {
+                    cb(err, player.id);
+                });
+            } else cb(err, player.id)
+        },
+        function (id, cb) {
+            uid = id;
+            var connectors = self.app.getServersByType('connector');
+            console.log("connectors:" + connectors);
+            console.log("connectorslen:" + connectors.length);
+            if (!connectors || connectors.length === 0) {
+                next(null, {
+                    code: 500
+                });
+                return;
+            }
+            // select connector
+            var res = dispatcher.dispatch(uid, connectors);
+            host = res.host;
+            port = res.clientPort;
+            cb(err);
+        }], function (err) {
+            if (err) {
+                next(err, { code: 501 });
+                return;
+            }
+            next(null, { code: 200, uid: uid, host: host, port: port });
+        }
+    );
 };
