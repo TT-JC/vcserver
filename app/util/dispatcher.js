65758a78a605290ae727ab43e42e7103762a97ca
TT-JC <1016399120@qq.com>
2020-07-30 01:05:31 +0800

    debug

--- app/util/dispatcher.js
+++ app/util/dispatcher.js
@@ -4,7 +4,7 @@ module.exports.dispatch = function (uid, connectors) {
     console.info("connectors:" + connectors);
     var count = connectors.length;
     console.info("count=", count);
-    console.info("uid:" + uid);
+    console.info(uid);
     var index = Math.abs(crc.crc32(uid)) % connectors.length;
     return connectors[index];
 };
